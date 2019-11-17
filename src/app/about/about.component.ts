81b5714fd5d42804503c2948c9807a094775303f
madhukar chouhan <madhukarsingh11@gmail.com>
2019-11-17 22:53:05 +0530

    Update site

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
-
+status = [
+{
+  "title": "HTML & CSS",
+  "percentage": 89
+},
+{
+  "title": "JAVASCRIPT & JQUERY",
+  "percentage": 59
+},
+{
+  "title": "ANGULARJS & ANGULAR",
+  "percentage": 52
+},
+{
+  "title": "ANGULAR MATERIAL & BOOTSTRAP",
+  "percentage": 70
+}
+];
   ngOnInit() {
   }
 
